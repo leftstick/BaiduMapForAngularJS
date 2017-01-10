bf8a646620965e849482c5f4a65e3b752c2b5130
Howard.Zuo <leftstick@qq.com>
2017-01-10 17:32:06 +0800

    fix spell issue

--- demo/js/fw/ext/main.js
+++ demo/js/fw/ext/main.js
@@ -1,6 +1,6 @@
 import 'angular-route';
 import 'highlight.js';
-import 'highlight.js/styles/darkula.css';
+import 'highlight.js/styles/darcula.css';
 import {ngBaiduMap} from '../../../../dist/angular-baidu-map';
 
 export default ['ngRoute', ngBaiduMap];
