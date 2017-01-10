1971e02e7a0bfafc2585824c969c05064914fb09
Howard.Zuo <leftstick@qq.com>
2017-01-10 11:20:53 +0800

    add global preset

---
+++ src/helper/preset.js
@@ -0,0 +1,50 @@
+
+
+export function globalConstants() {
+    //ControlAnchor
+    window.BMAP_ANCHOR_TOP_LEFT = 0;
+    window.BMAP_ANCHOR_TOP_RIGHT = 1;
+    window.BMAP_ANCHOR_BOTTOM_LEFT = 2;
+    window.BMAP_ANCHOR_BOTTOM_RIGHT = 3;
+
+    //NavigationControlType
+    window.BMAP_NAVIGATION_CONTROL_LARGE = 0;
+    window.BMAP_NAVIGATION_CONTROL_SMALL = 1;
+    window.BMAP_NAVIGATION_CONTROL_PAN = 2;
+    window.BMAP_NAVIGATION_CONTROL_ZOOM = 3;
+
+    //LengthUnit
+    window.BMAP_UNIT_METRIC = 'metric';
+    window.BMAP_UNIT_IMPERIAL = 'us';
+
+    //MapTypeControlType
+    window.BMAP_MAPTYPE_CONTROL_HORIZONTAL = 0;
+    window.BMAP_MAPTYPE_CONTROL_DROPDOWN = 1;
+    window.BMAP_MAPTYPE_CONTROL_MAP = 2;
+
+
+    //Animation
+    window.BMAP_ANIMATION_DROP = 1;
+    window.BMAP_ANIMATION_BOUNCE = 2;
+
+    //SizeType
+    window.BMAP_POINT_SIZE_TINY = 1;
+    window.BMAP_POINT_SIZE_SMALLER = 2;
+    window.BMAP_POINT_SIZE_SMALL = 3;
+    window.BMAP_POINT_SIZE_NORMAL = 4;
+    window.BMAP_POINT_SIZE_BIG = 5;
+    window.BMAP_POINT_SIZE_BIGGER = 6;
+    window.BMAP_POINT_SIZE_HUGE = 7;
+
+    //PanoramaSceneType
+    window.BMAP_PANORAMA_INDOOR_SCENE = 'inter';
+    window.BMAP_PANORAMA_STREET_SCENE = 'street';
+
+    //PanoramaPOIType
+    window.BMAP_PANORAMA_POI_HOTEL = 'hotel';
+    window.BMAP_PANORAMA_POI_CATERING = 'catering';
+    window.BMAP_PANORAMA_POI_MOVIE = 'movie';
+    window.BMAP_PANORAMA_POI_TRANSIT = 'transit';
+    window.BMAP_PANORAMA_POI_INDOOR_SCENE = 'indoor_scene';
+    window.BMAP_PANORAMA_POI_NONE = 'none';
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import baiduMap from './components/baiduMap';
 import marker from './components/marker';
 import control from './components/control';
 
+import {globalConstants} from './helper/preset';
+
+globalConstants();
+
 const moduleName = 'baiduMap';
 
 angular
