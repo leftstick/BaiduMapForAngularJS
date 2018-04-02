f18161d7363632e207bc667d140d3ed34fd66233
Howard.Zuo <leftstick@qq.com>
2018-04-02 10:08:36 +0800

    Finish: enhance undefined check while props changed in polyline

--- src/components/polyline.js
+++ src/components/polyline.js
@@ -1,4 +1,4 @@
-import { nullCheck } from '../helper/validate'
+import { nullCheck, isUndefined } from '../helper/validate'
 import { transformPoints } from '../helper/transformer'
 
 export default {
@@ -44,13 +44,26 @@ export default {
       if (changes.points && changes.points.currentValue) {
         this.polyline.setPath(transformPoints(changes.points.currentValue, '<polyline> points'))
       }
-      if (changes.options && changes.options.currentValue) {
+      if (!changes.options || !changes.options.currentValue) {
+        return
+      }
+      if (!isUndefined(changes.options.currentValue.strokeColor)) {
         this.polyline.setStrokeColor(changes.options.currentValue.strokeColor)
+      }
+      if (!isUndefined(changes.options.currentValue.strokeWeight)) {
         this.polyline.setStrokeWeight(changes.options.currentValue.strokeWeight)
+      }
+      if (!isUndefined(changes.options.currentValue.strokeOpacity)) {
         this.polyline.setStrokeOpacity(changes.options.currentValue.strokeOpacity)
+      }
+      if (!isUndefined(changes.options.currentValue.strokeStyle)) {
         this.polyline.setStrokeStyle(changes.options.currentValue.strokeStyle)
+      }
 
+      if (!isUndefined(changes.options.currentValue.enableMassClear)) {
         this.polyline[changes.options.currentValue.enableMassClear ? 'enableMassClear' : 'disableMassClear']()
+      }
+      if (!isUndefined(changes.options.currentValue.enableEditing)) {
         this.polyline[changes.options.currentValue.enableEditing ? 'enableEditing' : 'disableEditing']()
       }
     }
